da3f633d5fc1b79e304e10645ffa6c06d815148e
justalexh <alexhuynh0017@outlook.com>
2023-06-27 14:20:04 -0700

    Updated comments

--- Week 1/script.js
+++ Week 1/script.js
@@ -4,7 +4,7 @@
     var yearsOfStudy = 0;
     var goal = "pursue a career in backend development or a techincal project manager role";
 
-//Use write() to add newline instead of writeIn()
+//Use write() to add inline instead of writeIn()
     document.write("My name is " + firstName + " " + lastName + ". ");
     document.write("I have been at SCC for " + yearsOfStudy + " years ");
     document.write("and plan to " + goal + " after completing my courses.");
