09efc9a3bdd6dbc4a1a1ce8f728ab8d4d6d7138c
justalexh <alexhuynh0017@outlook.com>
2023-06-27 14:17:23 -0700

    Fixed Directory

--- Week 1/script.js
+++ Week 1/script.js
@@ -1,11 +1,11 @@
 //JS Script for description of myself.
-var firstName = "Alex";
-var lastName = "Huynh";
-var yearsOfStudy = 0;
-var goal = "pursue a career in backend development or a techincal project manager role";
+    var firstName = "Alex";
+    var lastName = "Huynh";
+    var yearsOfStudy = 0;
+    var goal = "pursue a career in backend development or a techincal project manager role";
 
 //Use write() to add newline instead of writeIn()
-document.write("My name is " + firstName + " " + lastName + ". ");
-document.write("I have been at SCC for " + yearsOfStudy + " years ");
-document.write("and plan to " + goal + " after completing my courses.");
-  
+    document.write("My name is " + firstName + " " + lastName + ". ");
+    document.write("I have been at SCC for " + yearsOfStudy + " years ");
+    document.write("and plan to " + goal + " after completing my courses.");
+    
