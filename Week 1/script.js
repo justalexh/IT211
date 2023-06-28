822c523bf858a878fdf44e76f8ea0240cbeeb46a
justalexh <alexhuynh0017@outlook.com>
2023-06-27 17:14:56 -0700

    Fixed Inline Script Placement

--- Week 1/script.js
+++ Week 1/script.js
@@ -1,11 +1,9 @@
 //JS Script for description of myself.
     const firstName = "Alex";
     const lastName = "Huynh";
-    let yearsOfStudy = 0;
+    let yearsOfStudy = "3 months";
     let goal = "pursue a career in backend development or a techincal project manager role";
 
-//Use write() to add inline instead of writeIn()
-    document.write("My name is " + firstName + " " + lastName + ". ");
-    document.write("I have been at SCC for " + yearsOfStudy + " years ");
-    document.write("and plan to " + goal + " after completing my courses.");
+
+    
     
